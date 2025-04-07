d2c8f264d47310335c8f90d99cfa7deba7097e1a
hobbybill <sungyool.park@gmail.com>
2025-04-07 15:07:24 -0700

    Category deletion fixed

--- api/routers/categories.js
+++ api/routers/categories.js
@@ -40,19 +40,44 @@ categoriesRouter.post("/", (req, res) => {
 });
 
 // see items.js for full explanation on how to create a DELETE endpoint
+// after some testing, I found that deleting a category with items in it will not delete the items
+// this is a problem because the items will still be in the database, but not in a category
+// I will add a delete items query before deleting the category
+// this will delete all items in the category before deleting the category
+// this is not the best way to do this, but it works for now
 categoriesRouter.delete('/:id', (req, res) => {
     // Extract category ID from the URL
     const { id } = req.params;
 
-    // SQL query to delete the category with the specified ID
-    const sql = "DELETE FROM categories WHERE id = ?";
-    // This runs the query above, category name replace the ? placeholders in the query
-    db.query(sql, [id], (err, results) => {
-      if (err) {
-        console.error("Error deleting category:", err);
-        return res.status(500).send(err);
-      }
-      res.json({ message: "Category deleted successfully" });
+    // First delete all items in this category first
+
+    const deleteItemsSQL = "DELETE FROM items WHERE category_id = ?";
+    db.query(deleteItemsSQL, [id], (err, itemResults) => {
+        if (err) {
+            // Log the error and send a 500 response
+            console.error("Error deleting items:", err);
+            return res.status(500).send(err);
+        }
+
+        // Now with the items deleted, we can delete the category
+        // SQL query to delete the category
+        // This will delete the category with the given ID
+        // The ? placeholder will be replaced with the ID of the category to delete
+        const deleteCategorySQL = "DELETE FROM categories WHERE id = ?";
+        db.query(deleteCategorySQL, [id], (err, categoryResults) => {
+            if (err) {
+                console.error("Error deleting category:", err);
+                return res.status(500).send(err);
+            }
+            // If the category was deleted successfully, send a success message
+            // The affectedRows property contains the number of rows affected by the delete query
+            // If the category was not found, affectedRows will be 0
+            res.json({
+                message: "Category and its items deleted successfully",
+                itemsDeleted: itemResults.affectedRows, // Number of items deleted
+                categoryDeleted: categoryResults.affectedRows // Should be 1 if the category was found and deleted
+            });
+        });
     });
-  });
+});
 module.exports = categoriesRouter;
